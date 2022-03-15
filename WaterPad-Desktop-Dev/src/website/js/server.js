8d4316d7c88c25e866a3671c78be635be5ed5e9b
WaterWolf5918 <87424826+Nightowl2007W@users.noreply.github.com>
2022-03-15 10:35:13 -0400

    Replaced WSS with Socket.Io

--- WaterPad-Desktop-Dev/src/website/js/server.js
+++ WaterPad-Desktop-Dev/src/website/js/server.js
@@ -1,16 +1,10 @@
-var ip1 = self.location.hostname
-var ip = "ws://" + ip1 +":65525"
+var hostname = self.location.hostname
+var ip = "ws://" + hostname +":65525"
+import * as socketIO from './socket.io.js';
+
+const socket = io('ws://localhost:65525');
 
 
+function apple(button) {
 
-
-let water = true
-function apple(button){
-	const block = button
-		// alert(ip)
-		const sock = new WebSocket(ip);
-		sock.addEventListener('open', function (event){
-			sock.send(parseInt(block))
-			sock.close
-		})
-	}
+}
